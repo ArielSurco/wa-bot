235e277258053412fc63b28b6a0da02117a1b39b
ArielSurco <arielsurcoa@gmail.com>
2022-10-22 14:39:12 -0300

    Fix canUseBot validation

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -6,15 +6,16 @@ import Bot from '../models/Bot';
 import Command from '../models/Command';
 import { getMessageText, isGroup } from '../utils/messageUtils';
 import { GroupActionType } from '../constants/enums';
+import { isCreator } from '../utils/rols';
 
 export const receiveMsg = async (bot: Bot, msg: WAMessage) => {
   const msgText = getMessageText(msg.message);
   const user = await bot.getMessageUser(msg);
   const command = Command.getCommand(bot.getCommands(), msgText);
   const group = bot.getGroup(msg.key.remoteJid);
-  const cantUseBot = !user || (group && !group.isActive());
+  const canUseBot = (user && (user.active || isCreator(user.id))) || (group && group.isActive());
 
-  if (cantUseBot) {
+  if (!canUseBot) {
     const responseText = isGroup(msg.key.remoteJid)
       ? 'No se puede utilizar el bot en este grupo. El creador debe habilitar el bot en este grupo para que se pueda utilizar.'
       : 'No puedes utilizar el bot. Debes pertenecer a alguno de los grupos donde el bot está habilitado.';
